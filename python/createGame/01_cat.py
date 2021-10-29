a7e44590fe324baa3431ec3e2b896626304f9f1d
arasho <arasho0319@gmail.com>
2021-10-29 14:46:31 +0900

    猫の描画処理作成(細かい設定コメント付与)

--- python/createGame/01_cat.py
+++ python/createGame/01_cat.py
@@ -8,7 +8,9 @@ CAT_W = 16
 
 class App:
     def __init__(self):
+        # 描画するウィンドウの作成
         pyxel.init(WINDOW_W, WINDOW_H, caption="Hello Pyxel")
+        # 描画するイラストの指定(0~2までしか指定は出来ない)
         pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
         pyxel.image(1).load(0, 0, "assets/cat_16x16.png")
         pyxel.run(self.update, self.draw)
@@ -21,6 +23,20 @@ class App:
         pyxel.cls(0)
         # pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
-        pyxel.blt(75, 45, 1, 0, 0, CAT_W, CAT_H, 13)
+        # bltメソッド
+        # イラスト描画位置
+        x = 75
+        y = 45
+        # 指定イラストの選択
+        img = 1
+        # 指定イラストの領域始点位置
+        u = 0
+        v = 0
+        # 指定イラストのサイズ
+        w = CAT_W
+        h = CAT_H
+        # 抜きたい色
+        colkey = 13
+        pyxel.blt(x, y, img, u, v, w, h, colkey)
 
 App()
